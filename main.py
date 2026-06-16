ff9a18fff644ba2c5a0d326e1029ba71071ba0fd
sdfpzxc <samdik1705@gmail.com>
2026-06-16 22:08:05 +0300

    docs: detailed usage hint

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 
 def main():
     if len(sys.argv) < 2:
-        print("Usage: python main.py <command>")
+        print("Usage:\n  python main.py add 'title'\n  python main.py list\n  python main.py done ID\n  python main.py remove ID")
         return
     print(f"Got command: {sys.argv[1]}")
 
